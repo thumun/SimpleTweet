3bfd8903b99e942d56baac9b7e0c218dbd0b5e18
nehathumu <nthumu@haverford.edu>
2021-03-12 23:45:57 -0500

    adding files

---
+++ app/build/generated/source/buildConfig/debug/com/codepath/apps/restclienttemplate/BuildConfig.java
@@ -0,0 +1,16 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.codepath.apps.restclienttemplate;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.codepath.apps.restclienttemplate";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+  // Fields from default config.
+  public static final String CONSUMER_KEY = "8rXrCtv5DA7zuHuyjgX8Jme3G";
+  public static final String CONSUMER_SECRET = "ztTLXf5oH6g0qeKo45AJJxP8zfpofBoH0W5UGW5iNHk6h9KLy0";
+}
